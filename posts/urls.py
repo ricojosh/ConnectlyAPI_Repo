255d3e6df7bc13fa8ec51fb5823d84e05b219216
ricojosh <ricojosh.lr.jricohermozo@mmdc.mcl.edu.ph>
2026-02-05 16:44:34 +0800

    Enhancing the Connectly API

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserListCreate, PostListCreate, CommentListCreate
+
+urlpatterns = [
+    path('users/', UserListCreate.as_view(), name='user-list-create'),
+    path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
+]
